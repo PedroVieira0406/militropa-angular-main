7372d0bc0a0fb778971b0e7e1f54d1c93375a097
Pedro Viera <pedroinho680@gmail.com>
2024-12-12 22:35:01 -0300

    funcionario-endereço_update

--- src/app/components/endereco/endereco-form/endereco-form.component.ts
+++ src/app/components/endereco/endereco-form/endereco-form.component.ts
@@ -1,11 +1,13 @@
 import { NgIf } from '@angular/common';
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { Endereco } from '../../../models/endereco.model';
@@ -27,6 +29,7 @@ export class EnderecoFormComponent {
   constructor(private formBuilder: FormBuilder,
     private enderecoService: EnderecoService,
     private router: Router,
+        private snackBar: MatSnackBar,
     private activatedRoute: ActivatedRoute) {
       this.formGroup = this.formBuilder.group({
         id:[null],
@@ -75,43 +78,43 @@ export class EnderecoFormComponent {
   }
 
   cancelar(){
-    this.router.navigateByUrl('/admin/estados');
+    this.router.navigateByUrl('/admin/enderecos');
   }
+
+  showSnackbarTopPosition(content: any) {
+    this.snackBar.open(content, 'fechar', {
+      duration: 3000,
+      verticalPosition: "top",
+      horizontalPosition: "center"
+    });
+  }
+
 
   
   salvar() {
+    this.formGroup.markAllAsTouched();
     if (this.formGroup.valid) {
       const endereco = this.formGroup.value;
-      if (endereco.id ==null) {
-        this.enderecoService.insert(endereco).subscribe({
-          next: (enderecoCadastrado) => {
-            this.router.navigateByUrl('/enderecos');
-          },
-          error: (err) => {
-            console.log('Erro ao Incluir' + JSON.stringify(err));
-          }
-        });
-      } else {
-        this.enderecoService.update(endereco).subscribe({
-          next: (enderecoAlterado) => {
-            this.router.navigateByUrl('/enderecos');
-          },
-          error: (err) => {
-            console.log('Erro ao Editar' + JSON.stringify(err));
-          }
-        });
-      }
+
+      const operacao = endereco.id == null
+        ? this.enderecoService.insert(endereco)
+        : this.enderecoService.update(endereco);
+
+      operacao.subscribe({
+        next: () => this.router.navigateByUrl('/admin/enderecos'),
+        error: (error) => {
+          console.log('Erro ao Salvar' + JSON.stringify(error));
+          this.tratarErros(error);
+        }
+      });
     }
   }
-
-  excluir() {
+excluir() {
     if (this.formGroup.valid) {
       const endereco = this.formGroup.value;
       if (endereco.id != null) {
         this.enderecoService.delete(endereco).subscribe({
-          next: () => {
-            this.router.navigateByUrl('/enderecos');
-          },
+          next: () => this.router.navigateByUrl('/admin/enderecos'),
           error: (err) => {
             console.log('Erro ao Excluir' + JSON.stringify(err));
           }
@@ -120,5 +123,61 @@ export class EnderecoFormComponent {
     }
   }
 
+  getErrorMessage(controlName: string, errors: ValidationErrors | null | undefined): string {
+    if (!errors) {
+      return '';
+    }
+    for (const errorName in errors) {
+      if (errors.hasOwnProperty(errorName) && this.errorMessages[controlName][errorName]) {
+        return this.errorMessages[controlName][errorName];
+      }
+    }
+    return 'Campo inválido';
+  }
 
+  tratarErros(errorResponse: HttpErrorResponse) {
+    if (errorResponse.status === 400 && errorResponse.error?.errors) {
+      errorResponse.error.errors.forEach((validationError: any) => {
+        const formControl = this.formGroup.get(validationError.fieldName);
+        if (formControl) {
+          formControl.setErrors({ apiError: validationError.message });
+        }
+      });
+    } else if (errorResponse.status >= 500) {
+      alert('Erro interno do servidor.');
+    } else {
+      alert(errorResponse.error?.message || 'Erro genérico ao enviar o formulário.');
+    }
+  }
+
+  errorMessages: { [controlName: string]: { [errorName: string]: string } } = {
+    nome: {
+      required: 'NOME é obrigatório',
+      apiError: ' '
+    },
+    logradouro: {
+      required: 'Logradouro é obrigatório',
+      apiError: ' '
+    },
+    numero: {
+      required: 'Numero é obrigatório',
+      apiError: ' '
+    },
+    bairro: {
+      required: 'Bairro é obrigatória',
+      apiError: ' '
+    },
+    cep: {
+      required: 'Cep é obrigatório',
+      apiError: ' '
+    },
+    cidade: {
+      required: 'Cidade é obrigatória',
+      apiError: ' '
+    },
+    estado: {
+      required: 'Estado é obrigatório',
+      apiError: ' '
+    },
+  }
 }
--- src/app/components/funcionario/funcionario-form/funcionario-form.component.ts
+++ src/app/components/funcionario/funcionario-form/funcionario-form.component.ts
@@ -1,15 +1,17 @@
-import { NgIf } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatOption } from '@angular/material/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { Endereco } from '../../../models/endereco.model';
 import { Funcionario } from '../../../models/funcionario.model';
 import { Usuario } from '../../../models/usuario.model';
@@ -17,70 +19,105 @@ import { EnderecoService } from '../../../services/endereco.service';
 import { FuncionarioService } from '../../../services/funcionario.service';
 import { UsuarioService } from '../../../services/usuario.service';
 
-
 @Component({
   selector: 'app-funcionario-form',
   standalone: true,
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatButtonModule, NgIf, MatInputModule, MatCardModule, MatToolbarModule, MatOption],
+  imports: [ReactiveFormsModule, MatFormFieldModule,
+    MatInputModule, MatButtonModule, MatCardModule, MatToolbarModule,
+    RouterModule, MatSelectModule, NgIf,
+    MatIconModule, FormsModule, NgFor,],
   templateUrl: './funcionario-form.component.html',
   styleUrl: './funcionario-form.component.css'
 })
-export class FuncionarioFormComponent implements OnInit{
+export class FuncionarioFormComponent implements OnInit {
   formGroup: FormGroup;
   funcionarios: Funcionario[] = [];
   usuarios: Usuario[] = [];
-  enderecos: Endereco [] = [];
+  enderecos: Endereco[] = [];
 
   constructor(private formBuilder: FormBuilder,
     private funcionarioService: FuncionarioService,
     private usuarioService: UsuarioService,
     private enderecoService: EnderecoService,
+    private snackBar: MatSnackBar,
     private router: Router,
     private activatedRoute: ActivatedRoute,
     private dialog: MatDialog) {
-      this.formGroup = this.formBuilder.group({
-        id: [null],
-        nome: ['', Validators.required],
-        cpf: ['', Validators.required],
-        email: ['', Validators.required],
-        telefone: ['', Validators.required],
-        endereco:[null],
-        matricula: ['', Validators.required],
-        usuario:[null]
-        }
-      )
+    this.formGroup = this.formBuilder.group({
+      id: [null],
+      nome: ['', Validators.required],
+      cpf: ['', [Validators.required, Validators.pattern(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/)]],
+      email: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]],
+      telefone: ['', [Validators.required, Validators.pattern(/^(\(?\d{2}\)?\s?)?(\d{4,5}-\d{4})$/)]],
+      endereco: [null, Validators.required],
+      matricula: ['', Validators.required],
+      usuario: [null, Validators.required]
+    });
   }
 
   ngOnInit(): void {
-    this.usuarioService.findAll().subscribe(data1 =>{
-      this.usuarios = data1;
-      this.initializeForm();
-    })
-    this.enderecoService.findAll().subscribe(data2 =>{
-      this.enderecos = data2;
-      this.initializeForm();
-    })
+    this.usuarioService.findAll().subscribe(data => {
+      this.usuarios = data;
+    });
+    this.enderecoService.findAll().subscribe(data => {
+      this.enderecos = data;
+    });
+    this.initializeForm();
   }
 
-  initializeForm(): void{
+  initializeForm(): void {
     const funcionario: Funcionario = this.activatedRoute.snapshot.data['funcionario'];
 
-    //selecionando o usuario
-    const usuario = this.usuarios.find(usuario => usuario.id === (funcionario?.usuario?.id || null));
-    
-    //selecionando o endereco
-    const endereco = this.enderecos.find(endereco => endereco.id === (funcionario?.endereco?.id || null));
+    const usuario = this.usuarios.find(u => u.id === (funcionario?.usuario?.id || null));
+    const endereco = this.enderecos.find(e => e.id === (funcionario?.endereco?.id || null));
 
-    this.formGroup = this.formBuilder.group({
-      id:[(funcionario && funcionario.id) ? funcionario.id : null],
-      nome: [(funcionario && funcionario.nome) ? funcionario.nome : '', Validators.required],
-      cpf: [(funcionario && funcionario.cpf) ? funcionario.cpf : '', Validators.required],
-      email: [(funcionario && funcionario.email) ? funcionario.email : '', Validators.required],
-      endereco:[endereco],
-      telefone: [(funcionario && funcionario.telefone) ? funcionario.telefone : '', Validators.required],
-      matricula: [(funcionario && funcionario.matricula) ? funcionario.matricula : '', Validators.required],
-      usuario:[usuario]
+    this.formGroup.patchValue({
+      id: funcionario?.id || null,
+      nome: funcionario?.nome || '',
+      cpf: funcionario?.cpf || '',
+      email: funcionario?.email || '',
+      telefone: funcionario?.telefone || '',
+      endereco: endereco || null,
+      matricula: funcionario?.matricula || '',
+      usuario: usuario || null
     });
+  }
+
+  showSnackbarTopPosition(content: any) {
+    this.snackBar.open(content, 'fechar', {
+      duration: 3000,
+      verticalPosition: "top",
+      horizontalPosition: "center"
+    });
+  }
+
+  cancelar(){
+    this.router.navigateByUrl('/admin/funcionarios');
+  }
+
+
+  formatCpf() {
+    let cpf = this.formGroup.get('cpf')?.value.replace(/\D/g, ''); // Remove qualquer coisa que não seja número
+    if (cpf.length <= 3) {
+      this.formGroup.get('cpf')?.setValue(cpf);
+    } else if (cpf.length <= 6) {
+      this.formGroup.get('cpf')?.setValue(cpf.replace(/(\d{3})(\d{0,3})/, '$1.$2'));
+    } else if (cpf.length <= 9) {
+      this.formGroup.get('cpf')?.setValue(cpf.replace(/(\d{3})(\d{3})(\d{0,3})/, '$1.$2.$3'));
+    } else {
+      this.formGroup.get('cpf')?.setValue(cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{0,2})/, '$1.$2.$3-$4'));
+    }
+  }
+
+  formatTelefone() {
+    let telefone = this.formGroup.get('telefone')?.value.replace(/\D/g, ''); // Remove qualquer coisa que não seja número
+    if (telefone.length <= 2) {
+      this.formGroup.get('telefone')?.setValue(telefone);
+    } else if (telefone.length <= 6) {
+      this.formGroup.get('telefone')?.setValue(telefone.replace(/(\d{2})(\d{0,4})/, '($1) $2'));
+    } else {
+      this.formGroup.get('telefone')?.setValue(telefone.replace(/(\d{2})(\d{4})(\d{0,4})/, '($1) $2-$3'));
+    }
   }
 
 
@@ -89,12 +126,10 @@ export class FuncionarioFormComponent implements OnInit{
     if (this.formGroup.valid) {
       const funcionario = this.formGroup.value;
 
-      // selecionando a operacao (insert ou update)
       const operacao = funcionario.id == null
-      ? this.funcionarioService.insert(funcionario)
-      : this.funcionarioService.update(funcionario);
+        ? this.funcionarioService.insert(funcionario)
+        : this.funcionarioService.update(funcionario);
 
-      // executando a operacao
       operacao.subscribe({
         next: () => this.router.navigateByUrl('/admin/funcionarios'),
         error: (error) => {
@@ -110,19 +145,13 @@ export class FuncionarioFormComponent implements OnInit{
       const funcionario = this.formGroup.value;
       if (funcionario.id != null) {
         this.funcionarioService.delete(funcionario).subscribe({
-          next: () => {
-            this.router.navigateByUrl('/admin/funcionarios');
-          },
+          next: () => this.router.navigateByUrl('/admin/funcionarios'),
           error: (err) => {
             console.log('Erro ao Excluir' + JSON.stringify(err));
           }
         });
       }
     }
-  }
-
-  cancelar(){
-    this.router.navigateByUrl('/admin/estados');
   }
 
   getErrorMessage(controlName: string, errors: ValidationErrors | null | undefined): string {
@@ -134,56 +163,50 @@ export class FuncionarioFormComponent implements OnInit{
         return this.errorMessages[controlName][errorName];
       }
     }
-
-    return 'invalid field';
+    return 'Campo inválido';
   }
 
   tratarErros(errorResponse: HttpErrorResponse) {
-
-    if (errorResponse.status === 400) {
-      if (errorResponse.error?.errors) {
-        errorResponse.error.errors.forEach((validationError: any) => {
-          const formControl = this.formGroup.get(validationError.fieldName);
-
-          if (formControl) {
-            formControl.setErrors({apiError: validationError.message})
-          }
-
-        });
-      }
-    } else if (errorResponse.status < 400){
-      alert(errorResponse.error?.message || 'Erro genérico do envio do formulário.');
+    if (errorResponse.status === 400 && errorResponse.error?.errors) {
+      errorResponse.error.errors.forEach((validationError: any) => {
+        const formControl = this.formGroup.get(validationError.fieldName);
+        if (formControl) {
+          formControl.setErrors({ apiError: validationError.message });
+        }
+      });
     } else if (errorResponse.status >= 500) {
       alert('Erro interno do servidor.');
+    } else {
+      alert(errorResponse.error?.message || 'Erro genérico ao enviar o formulário.');
     }
-
   }
 
-  errorMessages: {[controlName: string]: {[errorName : string]:string }} = {
-    nome:{
-      required: 'Perfil é obrigatório',
-    },
-    cpf:{
-      required: 'Cpf é obrigatória',
-      minLength: 'O tamanho do Cpf é 11 digitos',
-      maxLength: 'O tannho do Cpf é 11 digitos',
+  errorMessages: { [controlName: string]: { [errorName: string]: string } } = {
+    cpf: {
+      required: 'CPF é obrigatório',
+      pattern: 'O CPF deve estar no formato 000.000.000-00',
       apiError: ' '
     },
-    Email:{
+    email: {
       required: 'Email é obrigatório',
+      pattern: 'O email deve ser válido',
       apiError: ' '
     },
-
-    Telefone:{
+    telefone: {
       required: 'Telefone é obrigatório',
+      pattern: 'O telefone deve estar no formato (99) 99999-9999 ou (99) 9999-9999',
       apiError: ' '
     },
-    Matricula:{
-      required: 'Matricula é obrigatória',
+    matricula: {
+      required: 'Matrícula é obrigatória',
       apiError: ' '
     },
-    Usuario:{
-      required: 'Usuario é obrigatório',
+    usuario: {
+      required: 'Usuário é obrigatório',
+      apiError: ' '
+    },
+    endereco: {
+      required: 'Endereço é obrigatório',
       apiError: ' '
     }
   }
