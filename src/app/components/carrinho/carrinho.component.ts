9cf3b1fc106a3e79c89a7c2a6f785d198b0f0b13
Pedro Viera <pedroinho680@gmail.com>
2024-12-14 04:34:26 -0300

    tentativa-pagamaento

--- src/app/components/carrinho/carrinho.component.ts
+++ src/app/components/carrinho/carrinho.component.ts
@@ -3,6 +3,11 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ItemCarrinho } from '../../models/item-carrinho';
 import { CarrinhoService } from '../../services/carrinho.service';
+import { AuthService } from '../../services/auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { PedidoService } from '../../services/pedido.service';
+import { Pedido } from '../../models/pedido.model';
+import { ItemPedido } from '../../models/itemPedido.model';
 
 @Component({
   selector: 'app-carrinho',
@@ -11,30 +16,51 @@ import { CarrinhoService } from '../../services/carrinho.service';
   templateUrl: './carrinho.component.html',
   styleUrl: './carrinho.component.css'
 })
-export class CarrinhoComponent implements OnInit{
+export class CarrinhoComponent implements OnInit {
   carrinhoItens: ItemCarrinho[] = [];
+  pedido: Pedido []=[];
+  ItemPedidos: ItemPedido[]=[];
+
 
   constructor(
     private carrinhoService: CarrinhoService,
-    private router: Router
+    private authService: AuthService,
+    private snackBar: MatSnackBar,
+    private pedidoService: PedidoService,
+    private router: Router,
   ) { }
 
   ngOnInit(): void {
     this.carrinhoService.carrinho$.subscribe(itens => {
       this.carrinhoItens = itens;
     });
+    
   }
 
-  removerItem(item: ItemCarrinho){
+  removerItem(item: ItemCarrinho) {
     this.carrinhoService.removerItem(item);
   }
 
-  calcularTotal(): number{
+  calcularTotal(): number {
     return this.carrinhoItens.reduce((total, item) => total + item.quantidade * item.preco, 0)
   }
 
-  finalizarCompra(){
-    //verificar se esta logado
+  showSnackbarTopPosition(content: any) {
+    this.snackBar.open(content, 'fechar', {
+      duration: 3000,
+      verticalPosition: "top",
+      horizontalPosition: "center"
+    });
+  }
+
+  finalizarCompra() {
+    const authToken = this.authService.getToken();
+    if (authToken != null) {
+      
+    }
+    else {
+      this.snackBar.open('Token nulo', 'Fechar', { duration: 3000 });
+    }
   }
 
 }
