993ab6dfae7a7aef6dd02d659753c6d3cffabbf6
Pedro Viera <pedroinho680@gmail.com>
2024-12-14 01:38:03 -0300

    deletando classes inuteis

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,17 +14,11 @@ import { clienteResolver } from './components/cliente/resolver/cliente.resolver'
 import { EnderecoFormComponent } from './components/endereco/endereco-form/endereco-form.component';
 import { EnderecoListComponent } from './components/endereco/endereco-list/endereco-list.component';
 import { enderecoResolver } from './components/endereco/resolver/endereco.resolver';
-import { EstadoFormComponent } from './components/estado/estado-form/estado-form.component';
-import { EstadoListComponent } from './components/estado/estado-list/estado-list.component';
-import { estadoResolver } from './components/estado/resolver/estado.resolver';
 import { FuncionarioFormComponent } from './components/funcionario/funcionario-form/funcionario-form.component';
 import { FuncionarioListComponent } from './components/funcionario/funcionario-list/funcionario-list.component';
 import { funcionarioResolver } from './components/funcionario/resolver/funcionario.resolver';
 import { LoginComponent } from './components/login/login.component';
 import { LoginClienteComponent } from './components/loginCliente/login.component';
-import { MunicipioFormComponent } from './components/municipio/municipio-form/municipio-form.component';
-import { MunicipioListComponent } from './components/municipio/municipio-list/municipio-list.component';
-import { municipioResolver } from './components/municipio/resolver/municipio.resolver';
 import { AdminTemplateComponent } from './components/template/admin-template/admin-template.component';
 import { UserTemplateComponent } from './components/template/user-template/user-template.component';
 import { usuarioResolver } from './components/usuario/resolver/usuario.resolver';
@@ -54,14 +48,6 @@ export const routes: Routes = [
             { path: '', redirectTo: 'login', pathMatch: 'full' },
 
             { path: 'login', component: LoginComponent, title: 'Login'},
-
-            { path: 'estados', component: EstadoListComponent, title: 'Lista de Estados', canActivate: [authGuard],},
-            { path: 'estados/new', component: EstadoFormComponent, title: 'Novo Estado' , canActivate: [authGuard],},
-            { path: 'estados/edit/:id', component: EstadoFormComponent, resolve: { estado: estadoResolver } , canActivate: [authGuard],},
-
-            { path: 'municipios', component: MunicipioListComponent, title: 'Lista de Municípios', canActivate: [authGuard], },
-            { path: 'municipios/new', component: MunicipioFormComponent, title: 'Novo Município' , canActivate: [authGuard],},
-            { path: 'municipios/edit/:id', component: MunicipioFormComponent, resolve: { municipio: municipioResolver }, canActivate: [authGuard], },
 
             { path: 'armas', component: ArmaListComponent, title: 'Lista de Armas', canActivate: [authGuard], },
             { path: 'armas/new', component: ArmaFormComponent, title: 'Cadastrar Arma', canActivate: [authGuard], },
