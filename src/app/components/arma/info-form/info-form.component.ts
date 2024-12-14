0a9bc09a105f7e1ff77335874ca7d853de2e86bd
Pedro Viera <pedroinho680@gmail.com>
2024-12-13 22:35:49 -0300

    adicioando/removendo classes

---
+++ src/app/components/arma/info-form/info-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './info-form.component.html',
+  styleUrl: './info-form.component.css'
+})
+export class InfoFormComponent {
+
+}
