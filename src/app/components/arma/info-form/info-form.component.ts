78b0ed26fcd8a0b48fc4294dc74eb3efad7d3b2a
Pedro Viera <pedroinho680@gmail.com>
2024-12-14 00:29:39 -0300

    info-form

--- src/app/components/arma/info-form/info-form.component.ts
+++ src/app/components/arma/info-form/info-form.component.ts
@@ -1,12 +1,65 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MatCard, MatCardActions, MatCardContent, MatCardTitle } from '@angular/material/card';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
+import { Arma } from '../../../models/arma.model';
+import { AcabamentoService } from '../../../services/acabamento.service';
+import { ArmaService } from '../../../services/arma.service'; // Serviço para obter dados da arma (se houver)
+import { CarrinhoService } from '../../../services/carrinho.service';
 
 @Component({
-  selector: 'app-info-form',
+  selector: 'app-info-detalhes',
   standalone: true,
-  imports: [],
+  imports: [NgIf, MatCard, MatCardTitle, MatCardContent,MatCardActions],
   templateUrl: './info-form.component.html',
-  styleUrl: './info-form.component.css'
+  styleUrls: ['./info-form.component.css',]
 })
-export class InfoFormComponent {
+export class InfoFormComponent implements OnInit {
+  arma: any = {}; // Arma a ser exibida
+  armaId: string = '';
 
+  constructor(
+    private route: ActivatedRoute,
+    private armaService: ArmaService,
+    private carrinhoService: CarrinhoService,
+    private acabamentoService: AcabamentoService,
+    private snackBar: MatSnackBar,
+  ) {}
+
+  ngOnInit(): void {
+    // Obtém o ID da arma da URL
+    this.armaId = this.route.snapshot.paramMap.get('id') || '';
+    this.acabamentoService.findAll().subscribe(data => {
+      this.arma.acabamentoIds = data;
+    })
+    this.loadArmaDetalhes();
+  }
+
+  loadArmaDetalhes(): void {
+    // Chama o serviço para obter os dados da arma usando o ID
+    this.armaService.findById(this.armaId).subscribe((data) => {
+      this.arma = data; // Armazena as informações da arma
+      // Chama o serviço para pegar a URL da imagem, passando o nome da imagem
+      this.arma.imageUrl = this.armaService.getUrlImage(data.nomeImagem);
+    });
+  }
+
+  adicionarAoCarrinho(card: Arma) {
+    this.showSnackbarTopPosition('Produto adicionado ao carrinho');
+    this.carrinhoService.adicionar({
+      id: card.id,
+      nome: card.nome,
+      preco: card.preco,
+      quantidade: 1
+    });
+  }
+
+  showSnackbarTopPosition(content: any) {
+    this.snackBar.open(content, 'fechar', {
+      duration: 3000,
+      verticalPosition: "top",
+      horizontalPosition: "center"
+    });
+  }
 }
